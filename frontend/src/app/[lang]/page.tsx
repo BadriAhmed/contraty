6cec9d881ccb44d081f3ece5747abbf70d0944f7
BadriAhmed <abadri90@gmail.com>
2026-08-15 18:09:56 +0200

    fix(web): remove grey box around feature illustrations

--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -296,7 +296,7 @@ export default async function V2HomePage({
                 </div>
                 {/* Visual */}
                 <div className="flex-1 max-w-sm w-full">
-                  <div className="aspect-[4/3] rounded-2xl overflow-hidden border border-outline-variant/40 bg-surface">
+                  <div className="aspect-[4/3] overflow-hidden">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                       src={`/images/features/${feat.variant}.png`}
